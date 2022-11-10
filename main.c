8a4637707437c55d904c9e047a6ce3bc709f2c29
knuos <112151897+knuos@users.noreply.github.com>
2022-11-10 15:42:52 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,48 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_QUEUE_SIZE 5
-typedef struct ListNode{
-	int data;
-	struct ListNode *link;
-}ListNode;
+typedef int element;
+typedef struct TreeNode {
+	element key;
+	struct TreeNode *left, *right;
+} TreeNode;
 
-int search(ListNode *list, int value){
-//fill out the code below
-	int count = 0;
-	
-	return count;
-}
-
-ListNode* insert_last(ListNode* head, int data){
-	ListNode *node = (ListNode *)malloc(sizeof(ListNode));
-	node->data = data;
-	if (head == NULL) {
-		head = node;
-		node->link = head;
-	}
-	else {
-		node->link = head->link;	
-		head->link = node;		
-		head = node;		
-	}
-	return head;
-}
-int main(void)
+TreeNode * new_node(int item)
 {
-	ListNode *head = NULL;
-	int tmp1, tmp2=0;
-	printf("원형 연결리스트에 5개의 정수를 삽입하시오.\n");
-	for(int i=0; i<MAX_QUEUE_SIZE; i++){
-		scanf("%d",&tmp);
-		head = insert_last(head, tmp1);
+	TreeNode * temp = (TreeNode *)malloc(sizeof(TreeNode));
+	temp->key = item;
+	temp->left = temp->right = NULL;
+	return temp;
+}
+
+TreeNode * insert_node(TreeNode * node, int key)
+{
+	if (node == NULL) return new_node(key);
+
+	if (key < node->key)
+		node->left = insert_node(node->left, key);
+	else if (key > node->key)
+		node->right = insert_node(node->right, key);
+
+	return node;
+}
+
+int min(TreeNode *root){
+	\\아래에 코드를 작성하시오.
+}
+
+int max(TreeNode *root){
+	\\아래에 코드를 작성하시오.
+}
+
+int main()
+{
+
+	TreeNode * root = NULL;
+	int temp;
+
+	printf("이진 탐색 트리에 5개의 정수를 삽입 하시오.\n");
+	for(int i=0; i<5; i++){
+		scanf_s("%d", &temp);
+		insert_node(root, temp);
 	}
 	
-	printf("삽입한 원소중 찾고자 하는 값을 입력하시오.");
-	scanf("%d",&tmp1);
-	tmp2 = search(head, tmp1);
-	
-	printf("%d은 %d번째 노드에 저장되어 있음\n", tmp1, tmp2);
-	
+	printf("최소값: %d, 최대값: %d\n", min(root), max(root));
+		
 	return 0;
 }
